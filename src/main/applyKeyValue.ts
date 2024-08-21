6d257d0857237db97c5b251a08bee11e9ffa0f81
Ryo Nakae <ryo.n.1129@gmail.com>
2024-08-21 10:26:54 +0900

    apply keyValue

--- src/main/applyKeyValue.ts
+++ src/main/applyKeyValue.ts
@@ -1,6 +1,7 @@
 import type { NotionKeyValue } from '@/types/common'
+import { loadFontsAsync } from '@create-figma-plugin/utilities'
 
-export default function applyKeyValue(keyValue: NotionKeyValue) {
+export default async function applyKeyValue(keyValue: NotionKeyValue) {
   console.log('applyKeyValue', keyValue)
 
   // 何も選択していない場合は処理を終了
@@ -27,4 +28,23 @@ export default function applyKeyValue(keyValue: NotionKeyValue) {
     figma.notify('No text layers in selection.')
     return
   }
+
+  // 事前にフォントをロード
+  await loadFontsAsync(textNodes).catch((error: Error) => {
+    const errorMessage = 'Error on loading font.'
+    figma.notify(errorMessage, { error: true })
+    throw new Error(errorMessage)
+  })
+
+  // textNodeごとに処理を実行
+  textNodes.forEach(textNode => {
+    // レイヤー名を# + keyPropertyにする
+    textNode.name = `#${keyValue.key}`
+
+    // テキスト本文をvaluePropertyにする
+    textNode.characters = keyValue.value
+  })
+
+  // 完了通知
+  figma.notify('Applied key & value to selected text.')
 }
